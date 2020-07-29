58b31c17cee6b28bcde05dc2f55ce449245c66c4
Rocky-lappy <kev.rocky@gmail.com>
2020-07-29 08:00:53 -0700

    2 - added view folder

---
+++ view/src/pages/profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-ons-page>
+    <p style="text-align: center">This is the profile.</p>
+  </v-ons-page>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
